0030ebeb344bf56d37bbbf9609c0376a690fda77
Antoni Gawron <antek.gawron@gmail.com>
2026-02-27 22:38:06 +0100

    add astro deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-});
+  site: "https://forbiddenrng.github.io",
+  base: "/portfolio",
+})
